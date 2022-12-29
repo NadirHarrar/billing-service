ba833eb64e5d1c78447ea2dba84a91d32dbfcb94
Nadirouss <nadirharrar71@gmail.com>
2022-12-29 15:03:35 +0100

    second commit

--- src/main/java/org/sid/billingservice/entities/ProductItem.java
+++ src/main/java/org/sid/billingservice/entities/ProductItem.java
@@ -25,3 +25,4 @@ public class ProductItem {
         return price*quantity;
     }
 }
+
--- src/main/java/org/sid/billingservice/web/BillingRestController.java
+++ src/main/java/org/sid/billingservice/web/BillingRestController.java
@@ -7,13 +7,11 @@ import org.sid.billingservice.model.Customer;
 import org.sid.billingservice.model.Product;
 import org.sid.billingservice.repository.BillRepository;
 import org.sid.billingservice.repository.ProductItemRepository;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller @ResponseBody
+@RestController
 public class BillingRestController {
     private BillRepository billRepository;
     private ProductItemRepository productItemRepository;
